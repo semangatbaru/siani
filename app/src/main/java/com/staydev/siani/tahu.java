45d117c87debc10e0b27acb954a86d9918dcbb9b
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-08-13 16:48:33 +0700

    bismillah

---
+++ app/src/main/java/com/staydev/siani/tahu.java
@@ -0,0 +1,13 @@
+package com.staydev.siani;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tahu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tahu);
+    }
+}
