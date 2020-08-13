20bc44bd28a80c4a2a3fbd5eddd7b6c394a3e4fe
dhenitgp12345 <44762412+dhenitgp12345@users.noreply.github.com>
2020-08-13 18:14:59 +0700

    baru

--- app/src/main/java/com/staydev/siani/MainActivity.java
+++ app/src/main/java/com/staydev/siani/MainActivity.java
@@ -2,7 +2,9 @@ package com.staydev.siani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(MainActivity.this, Beranda.class);
+        startActivity(intent);
     }
 }
